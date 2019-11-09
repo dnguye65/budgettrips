867e3d1fb9dc3763e5e551a4b9471caa7e305159
Duyen Nguyen <dnguye65@uncc.edu>
2019-11-09 18:44:35 -0500

    add signout

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ function onSignIn(googleUser) {
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
+
+function signOut(){
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function(){
+        $('.g-signin2').css("display","block");
+        $('.data').css("display","none");
+    });
+}
