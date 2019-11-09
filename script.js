2c10dc0f976d4673afba277000a99aa0ae8f2140
Duyen Nguyen <dnguye65@uncc.edu>
2019-11-09 18:23:54 -0500

    adding functiona.ity in js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function onSignIn(googleUser) {
     var profile=googleUser.getBasicProfile();
     $(".g-signin2").css("display", "none");
     $(".profile").css("display","block");
-    $("#usericon").attr('src', profile,getImageUrl());
+    $("#usericon").attr('src', profile.getImageUrl());
     $("#emailAdd").text(profile.getEmail());
   }
