4d06396a526027f834fee06c7eac8ce7096b2a65
Duyen Nguyen <dnguye65@uncc.edu>
2019-11-09 18:26:48 -0500

    adding functiona.ity in js

--- script.js
+++ script.js
@@ -4,4 +4,9 @@ function onSignIn(googleUser) {
     $(".profile").css("display","block");
     $("#usericon").attr('src', profile.getImageUrl());
     $("#emailAdd").text(profile.getEmail());
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
   }
