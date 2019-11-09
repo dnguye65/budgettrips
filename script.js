0176138f001de4517655cd56bd4ef15c2617be8e
Duyen Nguyen <dnguye65@uncc.edu>
2019-11-09 18:18:19 -0500

    adding function in js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
+    var profile=googleUser.getBasicProfile();
     $(".g-signin2").css("display", "none");
     $(".profile").css("display","block");
     $("#usericon").attr('src', profile,getImageUrl());
     $("#emailAdd").text(profile.getEmail());
-    console.log('Name: ' + profile.getName());
   }
