14e9030ebfd447c95a0357553237487fc4b0237f
Duyen Nguyen <dnguye65@uncc.edu>
2019-11-10 10:56:56 -0500

    Adding more to index

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function onSignIn(googleUser) {
     var profile=googleUser.getBasicProfile();
     $(".g-signin2").css("display","none");
+    $(".jumbotron").css("display","none")
     $("#profile").css("display","block");
     $("#usericon").attr('src', profile.getImageUrl());
     $("#emailAdd").text(profile.getEmail());
