06e7bc68b8c730fc95599dc5d8f65e7b38244884
Duyen Nguyen <dnguye65@uncc.edu>
2019-11-09 21:22:56 -0500

    add signout modification

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function onSignIn(googleUser) {
     var profile=googleUser.getBasicProfile();
     $(".g-signin2").css("display", "none");
-    $(".profile").css("display","block");
+    $("#profile").css("display","block");
     $("#usericon").attr('src', profile.getImageUrl());
     $("#emailAdd").text(profile.getEmail());
     $("#name").text(profile.getName());
