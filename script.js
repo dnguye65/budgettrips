ba5842c482388d9092794f2d4e07d70cfe4d950b
Duyen Nguyen <dnguye65@uncc.edu>
2019-11-09 22:59:10 -0500

    Adding more to index

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function signOut(){
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function(){
         $('.g-signin2').css("display","block");
-        $('.profile').css("display","none");
+        $('#profile').css("display","none");
     });
 }
 
