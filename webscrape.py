781c7a5f9e56783fa9c7dfce74ebfe8bdc5e9028
Spencer Gillette <sgillet4@uncc.edu>
2019-12-07 16:57:01 -0500

    Delete webscrape.py

--- webscrape.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template, request
-app = Flask(__name__)
-
-@app.route('/search_deal', methods=['POST'])
-def get_input():
-    origin = request.forms.get('origin')
-    destination = request.forms.get('destination')
-    print(origin)
